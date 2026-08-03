8ca8575cc950456ce482a5ade5a0ebea65a07c31
r0der <calvorodrigoo@gmail.com>
2026-08-02 22:39:22 -0300

    newmodel

--- api/info-ia.js
+++ api/info-ia.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
 
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-2.5-flash-lite"
+        model: "gemini-2.5-flash"
     });
 
 
