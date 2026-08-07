ba0c9fec98448f99fc4495b2f8165b1e26b2d447
r0der <calvorodrigoo@gmail.com>
2026-08-06 23:48:33 -0300

    corregir cors

--- api/buscar-productos.js
+++ api/buscar-productos.js
@@ -1,7 +1,9 @@
 import productos from '../data/productos-completo.json' with { type: 'json' };
 
 export default function handler(req, res) {
+
   res.setHeader('Access-Control-Allow-Origin', 'https://vizta.lat');
+
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   const { q } = req.query;
