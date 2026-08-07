db899a782eefd2f5761c5e0173d255e2c5b3f5dd
r0der <calvorodrigoo@gmail.com>
2026-08-06 23:28:10 -0300

    cors dobles

--- api/buscar.js
+++ api/buscar.js
@@ -1,6 +1,19 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://vizta.lat');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    const origin = req.headers.origin;
+
+    const allowedOrigins = [
+        'https://vizta.lat',
+        'https://www.vizta.lat'
+    ];
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
 
   const { q } = req.query;
   if (!q) return res.status(400).json({ error: 'Falta el parámetro q' });
